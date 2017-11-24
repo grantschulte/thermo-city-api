6b5dbd676c389f770be379e5591107d9c7b00081
Grant Schulte <schulte.grant@gmail.com>
2017-11-24 09:47:17 -0500

    Removes console.logs

--- src/routes/geo-data/index.js
+++ src/routes/geo-data/index.js
@@ -6,16 +6,12 @@ const apiKey  = process.env.GOOGLE_GEOCODE_API_KEY;
 router.get("/", (req, res, next) => {
   const url = `${apiRoot}?latlng=${req.query.coords}&result_type=neighborhood|street_address&key=${apiKey}`;
 
-  console.log("URL", url);
-
   request(url, (err, response, body) => {
     if (err) {
       return next(err);
     }
 
     let data = JSON.parse(body);
-
-    console.log("GEO DATA", data);
 
     if (!data.results.length) {
       return next(err);
